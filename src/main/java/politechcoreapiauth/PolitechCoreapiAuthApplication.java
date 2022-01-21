5f8b5dda258ee878a3d4766da89d500f8eb2ab53
nnaltakyan <narek.naltakyan@dataart.com>
2022-01-21 15:28:33 +0400

    Initial Commit

---
+++ src/main/java/politechcoreapiauth/PolitechCoreapiAuthApplication.java
@@ -0,0 +1,13 @@
+package politechcoreapiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolitechCoreapiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolitechCoreapiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/politechcoreapiauth/PolitechCoreapiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package politechcoreapiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolitechCoreapiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
