6e0c764334b8031c13a3bae89b44a3e9be572be7
nnaltakyan <narek.naltakyan@dataart.com>
2022-01-22 16:52:17 +0400

    add new plugins

--- src/main/java/politechcoreapiauth/PolitechCoreApiAuthApplication.java
+++ src/main/java/politechcoreapiauth/PolitechCoreApiAuthApplication.java
@@ -1,13 +1,27 @@
 package politechcoreapiauth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@EnableAsync
 @SpringBootApplication
-public class PolitechCoreApiAuthApplication {
+public class PolitechCoreApiAuthApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(PolitechCoreApiAuthApplication.class, args);
+	}
+
+	@PostConstruct
+	public void afterStart()
+	{
+		log.info("Application started");
 	}
 
 }
